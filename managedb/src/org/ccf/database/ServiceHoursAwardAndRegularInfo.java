87f30ad678674d1e11cc5010410a1744f6f5e63c
ashleysheng <ashleysheng@gmail.com>
2016-09-20 17:32:23 -0500

    Modified the sql query statement for historical service hours and award.

--- managedb/src/org/ccf/database/ServiceHoursAwardAndRegularInfo.java
+++ managedb/src/org/ccf/database/ServiceHoursAwardAndRegularInfo.java
@@ -10,7 +10,14 @@ public class ServiceHoursAwardAndRegularInfo {
 	private String[] serviceHoursRegularServiceHoursSortHeader ={"序號","組別","姓名","服務時數"};
 	private String[] serviceHoursRegularPersonalDetailHeader ={"","活動","月份","時數"};
 	//private String serviceHoursAwardSQL = "select 姓名 ,SUM(時數),可累計 from servicehours where 年  < ? and 可累計 = 'TRUE' GROUP BY (姓名)";
-	private String serviceHoursAwardSQL = "select A1.組別,A1.姓名,A2.加入年,A2.加入月,A2.LeftDuration, IFNULL(A3.歷年服務時數,0) as 歷年服務時數, A4.100年底家扶累計時數  AS 100年底家扶累計時數 , A5.101年前服務冊時數  AS 101年前服務冊時數 , (IFNULL(100年底家扶累計時數,0) + IFNULL(101年前服務冊時數,0)) AS 服務冊時數 from mgroup A1 left outer join personalinfo A2 on A1.姓名 = A2.姓名  left outer join (select 姓名 as 姓名 ,SUM(時數) as 歷年服務時數  from servicehours where 年  <= ? and 可累計 = 'TRUE' GROUP BY (姓名))A3 on A1.姓名 = A3.姓名   left outer join brochure A4 on A1.姓名 = A4.姓名  left outer join (select 姓名 as 姓名,SUM(時數) as 101年前服務冊時數  from servicehours where 年 >= 101 and 年 <= ? and 可累計 = 'TRUE' GROUP BY (姓名))A5 on A5.姓名 = A4.姓名  where A1.年度 = ? and (A1.組別 != '退隊') and (A1.組別 != '展友') ORDER BY A1.組別 ASC";
+	//private String serviceHoursAwardSQL = "select A1.組別,A1.姓名,A2.加入年,A2.加入月,A2.LeftDuration, IFNULL(A3.歷年服務時數,0) as 歷年服務時數, A4.100年底家扶累計時數  AS 100年底家扶累計時數 , A5.101年前服務冊時數  AS 101年前服務冊時數 , (IFNULL(100年底家扶累計時數,0) + IFNULL(101年前服務冊時數,0)) AS 服務冊時數 from mgroup A1 left outer join personalinfo A2 on A1.姓名 = A2.姓名  left outer join (select 姓名 as 姓名 ,SUM(時數) as 歷年服務時數  from servicehours where 年  <= ? and 可累計 = 'TRUE' GROUP BY (姓名))A3 on A1.姓名 = A3.姓名   left outer join brochure A4 on A1.姓名 = A4.姓名  left outer join (select 姓名 as 姓名,SUM(時數) as 101年前服務冊時數  from servicehours where 年 >= 101 and 年 <= ? and 可累計 = 'TRUE' GROUP BY (姓名))A5 on A5.姓名 = A4.姓名  where A1.年度 = ? and (A1.組別 != '退隊') and (A1.組別 != '展友') ORDER BY A1.組別 ASC";
+	private String serviceHoursAwardSQL = "select A1.組別,A1.姓名,A2.加入年,A2.加入月,A2.LeftDuration , IFNULL(A3.歷年服務時數,0) as 歷年服務時數, A4.100年底家扶累計時數  AS 100年底家扶累計時數 , IFNULL(A5.100年前拿到服務冊人的服務時數,0)  AS 100年前拿到服務冊人的服務時數 , A6.100年後拿到服務冊人的服務時數  AS 100年後拿到服務冊人的服務時數 ,(IFNULL(100年底家扶累計時數,0) + IFNULL(100年前拿到服務冊人的服務時數,0) + IFNULL(100年後拿到服務冊人的服務時數,0)) AS 服務冊時數  from mgroup A1 " +
+			"left outer join personalinfo A2 on A1.姓名 = A2.姓名 " + 
+			"left outer join (select 姓名 as 姓名 ,SUM(時數) as 歷年服務時數  from servicehours where 年  <= ? and 可累計 = 'TRUE' GROUP BY (姓名))A3 on A1.姓名 = A3.姓名  " + 
+			"left outer join brochure A4 on A1.姓名 = A4.姓名  " + 
+			"left outer join (select 姓名 as 姓名,SUM(時數) as 100年前拿到服務冊人的服務時數 from servicehours where 年 >= 101 and 年 <= ? and 可累計 = 'TRUE' GROUP BY (姓名))A5 on A5.姓名 = A4.姓名  and A4.領紀錄冊年 <= 100 " + 
+			"left outer join (select B1.姓名 as 姓名,SUM(B1.時數) as 100年後拿到服務冊人的服務時數  from servicehours B1, brochure C1 where B1.姓名 = C1.姓名   and (case when B1.月 < 10 then concat(B1.年,0,B1.月) else concat(B1.年,B1.月) end) >= (case when C1.領紀錄冊月 < 10 then concat(C1.領紀錄冊年,0,C1.領紀錄冊月) else concat(C1.領紀錄冊年,C1.領紀錄冊月) end) and  B1.年 <= ? and B1.可累計 = 'TRUE' GROUP BY (B1.姓名))A6 on A6.姓名 = A4.姓名  and A4.領紀錄冊年 >= 101 " +
+			"where A1.年度 = ? and (A1.組別 != '退隊') and (A1.組別 != '展友') ORDER BY A1.組別 ASC";
 	private String serviceHoursAwardPeronalDetailSQL = "select A1.年,SUM(A1.時數) as 時數,A2.獲獎  from servicehours A1 left outer join (select 姓名,年,GROUP_CONCAT(award separator ',') as 獲獎 from award where 姓名 = ? GROUP BY 年)A2 on A1.年 = A2.年 and A1.姓名 = A2.姓名  where A1.姓名 = ? and A1.年 <= ? and 可累計 = 'TRUE' GROUP by A1.年 ORDER BY (0+A1.年)";
 	private String serviceHoursRegularSQL = "select A1.組別,A1.姓名,A2.totalservice as totalservice,A3.meetinghrs as meetinghrs,(IFNULL(totalservice,0) + IFNULL(meetinghrs,0)) AS 總時數 ,IFNULL(A4.service時數,0) AS service時數 , IFNULL(A5.1to3時數,0) AS 1to3時數, IFNULL(A6.4to6時數,0) AS 4to6時數, IFNULL(A7.7to9時數,0) AS 7to9時數 , IFNULL(A8.10to12時數,0) AS 10to12時數 from mgroup A1 left outer join (select 姓名 as 姓名,SUM(時數) as totalservice from servicehours where 年 = ? GROUP by (姓名))A2 on A1.姓名=A2.姓名  left outer join (select 姓名 as 姓名,SUM(時數) as meetinghrs from meeting where 年 = ? GROUP by (姓名))A3 on A1.姓名=A3.姓名  left outer join (select 姓名 as 姓名,SUM(時數)as service時數  from servicehours where 年 = ? and 可累計 = 'TRUE' GROUP by (姓名))A4 on A1.姓名 = A4.姓名 " +
 			"left outer join (select 姓名 as 姓名,SUM(時數)as 1to3時數  from servicehours where 年 = ? and 月 <= '3' and 可累計 = 'TRUE' GROUP by (姓名))A5 on A1.姓名 = A5.姓名  " +
@@ -59,7 +66,7 @@ public class ServiceHoursAwardAndRegularInfo {
 			 //歷年服務時數
 			 myData[i][4] = dbdata[i][5];
 			 //服務冊時數
-			 myData[i][5] = dbdata[i][8];
+			 myData[i][5] = dbdata[i][9];
 		 }
 		/* for(int i=0; i<rows; i++ ){
 			 for (int j=0; j <serviceHoursAwardHeader.length;j++ ){
@@ -81,7 +88,8 @@ public class ServiceHoursAwardAndRegularInfo {
 			    db.pst = db.con.prepareStatement(serviceHoursAwardSQL);
 				db.pst.setInt(1,searchyear);
 				db.pst.setInt(2,searchyear);
-				db.pst.setString(3,groupyear);
+				db.pst.setInt(3,searchyear);
+				db.pst.setString(4,groupyear);
 				db.rs = db.pst.executeQuery();
 			/* db.stat = db.con.createStatement(); 
 			  db.rs = db.stat.executeQuery(serviceHoursAwardSQL);*/
